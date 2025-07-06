371488acb3442e44403934c741b0915b3031ebf3
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-06 10:50:24 +0530

    commit

--- AsymptoticNotations/PrimeCheck.cpp
+++ AsymptoticNotations/PrimeCheck.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 bool isPrime(int n) {
     if (n < 2) return false;
     for (int i = 2; i*i <= n; i++)
         if (n % i == 0) return false;
     return true;
 }
-
 int main() {
     cout << isPrime(29);
 }
