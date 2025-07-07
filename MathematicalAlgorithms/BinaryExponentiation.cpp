a7549765d600b42a333da3052879d15238be3018
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-07 18:23:21 +0530

    commit

---
+++ MathematicalAlgorithms/BinaryExponentiation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int binpow(int a, int b) {
+    int res = 1;
+    while (b) {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    cout << binpow(2, 10);
+}
