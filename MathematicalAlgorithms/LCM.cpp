5c1f1527a3c2c11e4d84872316ad32ea90ce8647
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-07 18:16:23 +0530

    commit

---
+++ MathematicalAlgorithms/LCM.cpp
@@ -0,0 +1,14 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    cout << lcm(4, 6);
+}
