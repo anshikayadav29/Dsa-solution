f8df7fa8a20b1de55089f9decd2c523fde8f3a62
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-08 09:28:38 +0530

    commit

---
+++ MathematicalAlgorithms/CountwayToReachstair.cpp
@@ -0,0 +1,12 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+int ways(int n) {
+    if (n <= 1) return 1;
+    return ways(n - 1) + ways(n - 2);
+}
+
+int main() {
+    cout << ways(4);
+    return 0;
+}
