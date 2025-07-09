9aeae1ab9fff424ecbef70699757ff89c51f84ec
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-09 09:16:46 +0530

    commit

---
+++ MathematicalAlgorithms/FindNthFibonacciNumber.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n) {
+    if (n <= 1) return n;
+    int a = 0, b = 1;
+    for (int i = 2; i <= n; i++) {
+        int t = a + b;
+        a = b;
+        b = t;
+    }
+    return b;
+}
+
+int main() {
+    cout << fib(10);
+}
