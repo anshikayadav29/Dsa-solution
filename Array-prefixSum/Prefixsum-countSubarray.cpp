2daffd66b1a1da09581f86ed874895deb0e5ac70
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-10 22:05:57 +0530

    commit

---
+++ Array-prefixSum/Prefixsum-countSubarray.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> a = {1, 2, 3};
+    int k = 3, count = 0;
+    for(int i = 0; i < a.size(); i++) {
+        int sum = 0;
+        for(int j = i; j < a.size(); j++) {
+            sum += a[j];
+            if(sum == k) count++;
+        }
+    }
+    cout << count;
+}
