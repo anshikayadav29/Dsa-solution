e8ee605e9ebd19228dd61876e540b543fb438f9d
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-10 21:42:02 +0530

    commit

---
+++ ArrayBasicOperation/RotateArray.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v = {1, 2, 3, 4};
+    rotate(v.begin(), v.begin() + 1, v.end());
+    for(int x : v) cout << x << " ";
+}
