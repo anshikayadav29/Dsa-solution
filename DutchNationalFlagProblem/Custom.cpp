6b65014072466c048104f8277ac0d5ccc1568bc3
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-15 22:38:33 +0530

    commit

---
+++ DutchNationalFlagProblem/Custom.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int> a = {3,1,2,3,2,1};
+    int pivot = 2;
+    int l = 0, m = 0, h = a.size()-1;
+    while(m <= h){
+        if(a[m] < pivot) swap(a[l++], a[m++]);
+        else if(a[m] == pivot) m++;
+        else swap(a[m], a[h--]);
+    }
+    for(int x : a) cout << x << " ";
+}
