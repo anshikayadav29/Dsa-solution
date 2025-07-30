49d6e71c5770413a7c443edb9d8fb57378e6e432
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-30 22:54:38 +0530

    commit

---
+++ STRINGPALINDROME/Countalll.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int countSubstrings(string s) {
+    int n = s.size(), count = 0;
+    for (int center = 0; center < 2 * n - 1; center++) {
+        int l = center / 2, r = l + center % 2;
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            count++; l--; r++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    string s = "aaa";
+    cout << countSubstrings(s);  // 6
+}
